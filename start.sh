533b5648b3873f8aa0cdb395829c143f653b6cdc
manurdead1-create <manurdead1@gmail.com>
2026-04-17 20:01:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start the TypeScript API in the background
 # This runs src/index.ts using ts-node
-ts-node src/index.ts &
+ts-node --project tsconfig.json src/index.ts &
 
 # Start Nginx in the foreground
 nginx -g "daemon off;"
